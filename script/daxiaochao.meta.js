e0ef78374e9a2a7db83b657f248d2158ff7e29e2
LK <zuqiuzuqiu@126.com>
2026-08-07 11:36:22 +0800

    首次提交

---
+++ script/daxiaochao.meta.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name         三国杀打小抄一将成名
+// @namespace    https://greasyfork.org/scripts/448004
+// @version      1.15.2
+// @author       小麦 孤独尊
+// @description  打小抄支持网页版和微端，微端文件请加群 830137048
+// @icon         https://i0.hdslb.com/bfs/new_dyn/17ec41a0ca79633b77399065ab80da3f2138912.png
+// @match        https://game.4399iw2.com/yxxsgs/*
+// @match        *://*.sanguosha.com/10/*
+// @match        *://*.sanguosha.com/x/*
+// @match        *://*.sanguosha.com/10th/*
+// @match        https://wan.baidu.com/*gameId=19793616*
+// @match        *://h5.7k7k.com/web/H5GAMES.html?gid=960982bec2f555de44ea43ca8a7ef418/*
+// @match        *://qqgame.qq.com/webappframe/?appid=10951
+// @match        *://s118.app1107877410.qqopenapp.com/pc/qqLobby_index.php*
+// @exclude      *://*.sanguosha.com/220/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
